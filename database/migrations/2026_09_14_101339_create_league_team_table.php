092a3ceb71e7dfb7b3cff3f10f3e17ae27dc77ab
hosbyte <hosbyte@gmail.com>
2026-09-14 14:51:12 +0330

    create database table

---
+++ database/migrations/2026_09_14_101339_create_league_team_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('league_team', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('league_id')
+                ->constrained('leagues')
+                ->cascadeOnDelete();
+
+            $table->foreignId('team_id')
+                ->constrained('teams')
+                ->cascadeOnDelete();
+
+            $table->timestamps();
+
+            $table->unique(['league_id', 'team_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('league_team');
+    }
+};
---
+++ database/migrations/2026_09_14_102004_create_matches_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('matches', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('league_id')
+                ->constrained('leagues')
+                ->cascadeOnDelete();
+
+            $table->foreignId('home_team_id')
+                ->constrained('teams')
+                ->cascadeOnDelete();
+
+            $table->foreignId('away_team_id')
+                ->constrained('teams')
+                ->cascadeOnDelete();
+
+            $table->unsignedInteger('home_score');
+
+            $table->unsignedInteger('away_score');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('matches');
+    }
+};
